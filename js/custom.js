d36c45826d454017da2c694edd39121ac4a3b806
Andrey Velychko <60106215+andriyvelychko@users.noreply.github.com>
2021-04-05 18:02:48 +0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -123,6 +123,43 @@ $(() => {
   });
 });
 //--------------------------------------------------
+//Gallery
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function(item) {
+        return item.el.attr('title');
+      }
+    }
+  });
+//--------------------------------------------------
+$('.popup-with-form').magnificPopup({
+    type: 'inline',
+    preloader: false,
+    focus: '#name',
+
+    // When elemened is focused, some mobile browsers in some cases zoom in
+    // It looks not nice, so we disable it:
+    callbacks: {
+      beforeOpen: function() {
+        if($(window).width() < 700) {
+          this.st.focus = false;
+        } else {
+          this.st.focus = '#name';
+        }
+      }
+    }
+  });
+//--------------------------------------------------
 new WOW().init();
 });
 
